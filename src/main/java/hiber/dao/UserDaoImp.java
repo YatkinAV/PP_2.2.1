8522467add71c158cdd40b2060d2ead591cfac1b
YatkinAV <127983928+YatkinAV@users.noreply.github.com>
2023-06-01 16:26:10 +0300

    Update UserDaoImp.java

--- src/main/java/hiber/dao/UserDaoImp.java
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -42,25 +42,4 @@ public class UserDaoImp implements UserDao {
         return (User) query.getSingleResult();
     }
 
-   /*@Override
-public User carList(String model, int series) {
-    Session session = sessionFactory.openSession();
-    Query query = session.createQuery("from User u where u.car.model=:model " +
-            "and u.car.series=:series", User.class);
-    query.setParameter("model", model);
-    query.setParameter("series", series);
-    try {
-        return (User) query.getResultList();
-    } catch (Exception e) {
-        System.out.println("Юзер с таким авто не найден.");
-        return null;
-    }
-
-
-    FROM User u JOIN u.car c WHERE c.model=:model " +
-            "and c.series=:series"
-
-3:14 PM
-насчет JOIN можно поиграться, LEFT JOIN FETCH или ещё чего, пробуй
-}*/
 }
